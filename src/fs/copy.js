f3016bcd65e3d3f697b54cbe579835818d3cf092
a-klimovich <kom9toshka@gmail.com>
2023-06-01 16:02:27 +0200

    fs::copy::add error message

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,9 +1,11 @@
 import fs from "fs";
 
+const error = "FS operation failed";
+
 const copy = async () => {
   fs.cp("src/fs/files", "src/fs/copyDirFiles", { recursive: true }, (err) => {
     if (err) {
-      console.error(err);
+      console.error(error);
     }
   });
 };

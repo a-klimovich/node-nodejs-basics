72352dd33851858f7e13f0adf7668b38a942c15b
a-klimovich <kom9toshka@gmail.com>
2023-06-01 16:00:09 +0200

    add:fs:: deep copy dir "files"

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,11 @@
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+  fs.cp("src/fs/files", "src/fs/copyDirFiles", { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
 
 await copy();
